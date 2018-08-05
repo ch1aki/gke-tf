4e2cf5536c64d26070246bd14286bf5805fd4aa7
akichan22 <sugawara256@gmail.com>
2018-08-05 22:56:35 +0900

    add comment to provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,5 @@
+// Provider setting to be provided via ENV
+//   * "GOOGLE_PROJECT": project id 
+//   * "GOOGLE_APPLICATION_CREDENTIALS": path to credentials.json
+//   * "GOOGLE_REGION": reagion(ex. asia-northeast1)
 provider "google" {}
