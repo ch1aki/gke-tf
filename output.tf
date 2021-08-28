e3f835906f89e317bbf0747310d3042ffa9ab69e
ch1aki <sugawara256@gmail.com>
2021-08-28 22:04:25 +0900

    update

--- output.tf
+++ output.tf
@@ -2,14 +2,6 @@ output "cluster_name" {
   value = "${google_container_cluster.primary.name}"
 }
 
-output "primary_zone" {
-  value = "${google_container_cluster.primary.zone}"
-}
-
-output "additional_zones" {
-  value = "${google_container_cluster.primary.additional_zones}"
-}
-
 output "endpoint" {
   value = "${google_container_cluster.primary.endpoint}"
 }
